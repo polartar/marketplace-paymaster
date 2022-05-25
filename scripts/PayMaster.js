19e14a2808dd617084ac8b82ad8f0bedd64369a4
talentdev2020 <polaris.center@outlook.com>
2022-05-25 11:14:46 -0700

    remove mock testing gsn contracts

--- scripts/PayMaster.js
+++ scripts/PayMaster.js
@@ -10,8 +10,7 @@ async function main() {
 
   console.log("paymaster deployed to:", paymaster.address);
 
-  // config paymaster
-
+  // config paymaster. This should be replaced
   await paymaster.setRelayHub("0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA");
   await paymaster.setTrustedForwarder("0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB");
   await paymaster.setTarget("0x6D394936E3abcD44b20A5E24280Cb4b781d07Bac");
